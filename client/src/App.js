381e3f980a61a0c55e5f6fb5e2410fcbcd0adc4c
darzumanyan <d.arzumanyan@smytsoft.com>
2022-11-26 14:53:36 +0400

    sign in page

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import SignIn from './components/Signin';
+
+const App = () => {
+  return (
+    <>
+      <SignIn />
+    </>
+  );
+};
+
+export default App;
