ba4fc0e3d708d2a755ef4c7e32ee56fd618d089f
yuraavakyan <yura372554@gmail.com>
2022-11-27 22:34:32 +0400

    add exam resukts endpoint and jwt authentication

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+require('dotenv').config({ path: require('find-config')('.env') })
 
 const app = express();
 
@@ -11,8 +13,34 @@ app.use(express.json());
 
 const users = [{
     username: 'username',
-    password: 'password'
+    password: bcrypt.hash('password', 10).toString()
 }];
+
+const examResults = [
+  {
+    username: 'username',
+    score: 9, 
+  },
+  {
+    username: 'hakob',
+    score: 5,
+  },
+  {
+    username: 'Nairi',
+    score: 8
+  }
+]
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]
+  if(!token) res.status(401).send('no token')
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if(err) return res.status (403).send('you are not authorized to see this page')
+    req.user = user;
+    next();
+  })
+}
 
 app.get('/users', (req, res) => {
   res.json(users);
@@ -33,15 +61,20 @@ app.post('/users/login', async (req, res) => {
     const {username, password} = req.body;
   const user = users.find((user) => user.username === username);
   if (!user) {
-    return res.status(400).send('user not found');
+    return res.status(500).send('username or passowrd is incorrect')
   }
-  try {
-    if (await bcrypt.compare(password, user.password)) {
-      res.status(200).send('Successfully logged in');
+    if (bcrypt.compare(password, user?.password)) {
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+      res.status(200).json({accessToken});
+    } else {
+      res.status(500).send('username or passowrd is incorrect')
     }
-  } catch {
-    res.status(500).send('Password is incorrect');
-  }
 });
+
+app.get('/get-exam-results', authenticateToken, (req, res) => {
+  res.json(examResults.filter(result => result.username === req.user.username))
+});
+
+
 
 app.listen(5000, () => console.log('server has started on 5000'));
